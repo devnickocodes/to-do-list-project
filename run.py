57f5fef5c6c113b9e043abe7515062fda3c27058
devnickocodes <>
2024-01-05 13:51:07 +0000

    add a function that deals with authentication and access to google sheets spreadsheet that will contain the tasks, retrieve it and return it as well

--- run.py
+++ run.py
@@ -12,13 +12,32 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('to-do-list-app')
 
-tasks = SHEET.worksheet('tasks')
 
-data = tasks.get_all_values()
+def authenticate_google_sheets():
+    """
+    Function that deals with authentication and access
+    to Google Sheets spreadsheet for the tasks, it includes
+    a try - except block that handles the issues if any in the
+    authentication process arise
+    """
+    try:
+        CREDS = Credentials.from_service_account_file('creds.json')
+        SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+        GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+        SHEET = GSPREAD_CLIENT.open('to-do-list-app')
+        tasks = SHEET.get_worksheet(0) 
+        worksheet = tasks.get_all_values()
+        return worksheet
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
-print(data)
+
+def validate():
+
+    worksheet = authenticate_google_sheets()
+    print(worksheet)
+
+
+validate()
