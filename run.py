a452ab233e9eb0d3d752afd2265ba5cfb5cc8173
Nick Hristev <devnickocodes@gmail.com>
2024-01-10 10:47:50 +0000

    change the styling of the intro in the title_text function

--- run.py
+++ run.py
@@ -182,7 +182,7 @@ def title_text():
     """
     Function that displays the styled intro and author texts
     """
-    intro = figlet_format("Welcome to your To-Do List !", font = "mini", width = 70, justify = 'center')
+    intro = figlet_format("Welcome to your To-Do List !", font = "serifcap", width = 70, justify = 'center')
     author = figlet_format("By Nikolay Hristev", font = "contessa", width = 80, justify = 'center')
     
     print(Fore.LIGHTBLUE_EX + Style.BRIGHT + intro)
